92b8809866aeec049ae84a31a8866a129162b314
stevenpfiggins <sfiggins43@gmail.com>
2019-01-14 10:34:23 -0500

    registration and login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from "@angular/forms";
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  private _loginForm: FormGroup;
+
+  constructor(private _form: FormBuilder, private _authService: AuthService) {
+    this.createForm();
+   }
+
+  ngOnInit() {
+  }
+
+  createForm() {
+    this._loginForm = this._form.group({
+      username: new FormControl,
+      password: new FormControl
+    });
+  }
+
+  onSubmit() {
+    this._authService.login(this._loginForm.value);
+  }
+
+}
