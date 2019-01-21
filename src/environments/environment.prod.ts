457ffbba102378f1fa841e4e88fbe8bf54f8706e
marcguerra91 <marcguerra91@gmail.com>
2019-01-21 15:25:14 -0500

    jsonchange

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export let Api_Url = '';
 
 switch (window.location.hostname) {
-  case 'easy-outings.herokuapp.com':
 
+  case 'easy-outings.herokuapp.com':
   Api_Url = 'https://easyoutings.azurewebsites.net'
   break;
 
   default:
-  Api_Url = 'https://localhost:44311'
+  Api_Url = 'https://easyoutings.azurewebsites.net'
 }
 
 export const environment = {
