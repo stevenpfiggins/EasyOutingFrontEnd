c38238eeb7d191c93f1f72f3b8e89799a44d9349
marcguerra91 <marcguerra91@gmail.com>
2019-01-21 13:40:14 -0500

    initialcommit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,11 @@ switch (window.location.hostname) {
 
   Api_Url = 'https://easyoutings.azurewebsites.net'
   break;
-  
+
   default:
   Api_Url = 'https://localhost:44311'
 }
+
+export const environment = {
+  production: false
+};
