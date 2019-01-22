119de6d0b2f47a1db7b0bd720f7c29b0c16a7235
stevenpfiggins <sfiggins43@gmail.com>
2019-01-22 13:58:40 -0500

    environment update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+export let Api_Url = '';
+
+switch (window.location.hostname) {
+
+  case 'easy-outings.herokuapp.com':
+  Api_Url = 'https://easyoutings.azurewebsites.net'
+  break;
+
+  default:
+  Api_Url = 'https://easyoutings.azurewebsites.net'
+}
+
 export const environment = {
   production: true
 };
